84ead9294ee9894159709ea8325b404186b3f588
abhay269 <70260307+abhay269@users.noreply.github.com>
2026-09-12 13:57:41 +0000

    chore(database): regenerate amenity contracts

--- database/prisma/schema.d.ts
+++ database/prisma/schema.d.ts
@@ -2109,6 +2109,17 @@ type ContractBase = Omit<
               };
             };
             readonly relations: {
+              readonly bookings: {
+                readonly to: {
+                  readonly namespace: 'public' & NamespaceId;
+                  readonly model: 'Booking';
+                };
+                readonly cardinality: '1:N';
+                readonly on: {
+                  readonly localFields: readonly ['id'];
+                  readonly targetFields: readonly ['residentId'];
+                };
+              };
               readonly complaints: {
                 readonly to: {
                   readonly namespace: 'public' & NamespaceId;
@@ -2256,6 +2267,17 @@ type ContractBase = Omit<
               };
             };
             readonly relations: {
+              readonly amenities: {
+                readonly to: {
+                  readonly namespace: 'public' & NamespaceId;
+                  readonly model: 'Amenity';
+                };
+                readonly cardinality: '1:N';
+                readonly on: {
+                  readonly localFields: readonly ['id'];
+                  readonly targetFields: readonly ['societyId'];
+                };
+              };
               readonly staff: {
                 readonly to: {
                   readonly namespace: 'public' & NamespaceId;
